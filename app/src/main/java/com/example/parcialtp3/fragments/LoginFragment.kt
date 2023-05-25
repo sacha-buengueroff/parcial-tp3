b5e53c68a239664dfeaddae271aa14d8bf6e73d6
sebastiankatz <sebikatzcatz@gmail.com>
2023-05-25 12:02:12 -0300

    home_fragment y slide menu

---
+++ app/src/main/java/com/example/parcialtp3/fragments/LoginFragment.kt
@@ -0,0 +1,33 @@
+package com.example.parcialtp3.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import com.example.parcialtp3.R
+
+class LoginFragment : Fragment() {
+
+    lateinit var fragmentView : View
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        fragmentView =  inflater.inflate(R.layout.login_fragment, container, false)
+
+        val btnLogin = fragmentView.findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {var action = LoginFragmentDirections.actionLoginFragmentToHomeFragment3()
+            fragmentView.findNavController().navigate(action) }
+        return fragmentView
+    }
+
+}

680368f617fd2d93654d1a9c90880bae9504597f
FedericoRohr <rohrfede@gmail.com>
2023-05-26 16:21:59 -0300

    Se elimino CarResponse debido a que era innecesario, ahora el JSON se mapea a objeto de dominio CAR

--- app/src/main/java/com/example/parcialtp3/domain/Car.kt
+++ app/src/main/java/com/example/parcialtp3/domain/Car.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Car (
     @SerializedName("class") val carClass: String, // Utiliza otro nombre para evitar la palabra reservada "class"
-    val fuel_type:String,
+    @SerializedName( "fuel_type")val fuelType:String,
     val make:String,
     val model:String,
     val transmission:String,
--- app/src/main/java/com/example/parcialtp3/model/response/CarResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.parcialtp3.model.response
-
-import com.example.parcialtp3.model.domain.Car
-
-data class CarResponse(
-    val results:List<Car>
-)

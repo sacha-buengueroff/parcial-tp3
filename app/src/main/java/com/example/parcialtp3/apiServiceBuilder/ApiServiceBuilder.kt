68078c2f5f27f0613a9d0e1d83aef8fb5f663943
FedericoRohr <rohrfede@gmail.com>
2023-05-26 14:46:48 -0300

    Se agrega interceptor para visualizar log

--- app/src/main/java/com/example/parcialtp3/apiServiceBuilder/ApiServiceBuilder.kt
+++ app/src/main/java/com/example/parcialtp3/apiServiceBuilder/ApiServiceBuilder.kt
@@ -1,13 +1,24 @@
 package com.example.parcialtp3.apiServiceBuilder
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object APIServicesBuilder {
     private val BASE_URL= "https://api.api-ninjas.com/v1"
 
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
--- app/src/main/java/com/example/parcialtp3/model/CarResponse.kt
+++ app/src/main/java/com/example/parcialtp3/model/CarResponse.kt
@@ -1,14 +1,15 @@
 package com.example.parcialtp3.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CarResponse(
     val results:List<Car>
 )
 data class Car (
+    @SerializedName("class") val carClass: String, // Utiliza otro nombre para evitar la palabra reservada "class"
     val fuel_type:String,
     val make:String,
     val model:String,
     val transmission:String,
     val year:Int
-    // TODO: revisar que atributo es el "Two seater" si fuera class como lo declaro? porque class es una palabra reservada y si no no lo mapea el gson
-
         )
